52b0643559397c20c553a5459372c3f3c9c3e064
Morozov-Alexander <morozov.alex.0001@gmail.com>
2019-04-24 14:38:10 +0300

    Beautiful_cod

--- Alexander/13.rb
+++ Alexander/13.rb
@@ -1,20 +1,19 @@
-a = {'a' => 1 , 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => 6, 'g' => 7, 'h' => 8}
+array = {'a' => 1 , 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => 6, 'g' => 7, 'h' => 8}
 
-puts("Вначале вводи букву а потом цифру(координаты доски для шахмат) \nи так для двух клеток!!!")
+puts("Вначале вводи букву , а потом цифру(координаты доски для шахмат) \nи так для двух клеток!!!")
 
-Geter1 =a[gets.chomp.downcase]
-FirstPole = gets.chomp.to_i;
-Geter2 =a[gets.chomp.downcase]
-TheSecondPole = gets.chomp.to_i
+geter1 =array[gets.chomp.downcase]
+first_pole = gets.chomp.to_i;
+geter2 =array[gets.chomp.downcase]
+the_second_pole = gets.chomp.to_i
 
-if !Geter1 || !Geter2 || !FirstPole.between?(1,8) || !TheSecondPole.between?(1,8)
+if !geter1 || !geter2 || !first_pole.between?(1,8) || !the_second_pole.between?(1,8)
 puts "Няма таких симвалау!!! \nТольки {a,b,c,d,e,f,g,h\nAnd numbers between(1..8)}!" ; return
 end  
-
-A =(FirstPole - TheSecondPole)
-B = (Geter1-Geter2)
+a =(first_pole - the_second_pole)
+b = (geter1-geter2)
   
-if A.abs + B.abs == 3
+if a.abs + b.abs == 3
      puts "Конь доскачет!!"
      
     else
