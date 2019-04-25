0f532fd18053a67331fc36d2a001e8261931c38e
Morozov-Alexander <morozov.alex.0001@gmail.com>
2019-04-25 23:29:40 +0300

    Some changes

--- Alexander/1.rb
+++ Alexander/1.rb
@@ -1,12 +1,12 @@
 #Сдвинуть элемент массива вправо на количество, равное его индексу. Размер массива не должен меняться, а количество сдвигов равно количеству элементов массива
-array= []
+array = []
 (5 + rand(15)).times{ |i| array.push(i+1) }
-puts"Надо ввести номер элемента, по индексу которого будет сдвиг.\nРазмер массива = #{ array.size }"
+puts "Надо ввести номер элемента, по индексу которого будет сдвиг.\nРазмер массива = #{ array.size }"
 indeks = gets.chomp.to_i
 if !indeks.between?(1, array.size)
-  p 'No, out of bounds array!!!'
+  p "No, out of bounds array!!!"
   return
 end
-p array.to_s + "---Начальный массив."
-array.shift((indeks - array.size - 1).abs).each{ |i| array.push(i) }
-p array.to_s + "---Массив после сдвига."
+p array.to_s + "---Начальный массив"
+array.shift((indeks - array.size - 1).abs).each { |i| array.push(i) }
+p array.to_s + "---Массив после сдвига"
--- Alexander/16.rb
+++ Alexander/16.rb
@@ -1,7 +1,6 @@
 #Понять местами ключ-значения в хэше.
 hash = {}
  (1..10).each { |i| hash[i] = rand(100) }
-
-p hash.to_s + '---Было'
-hash =hash.invert
-p hash.to_s + '---Стало'
+p hash.to_s + "---Было"
+hash = hash.invert
+p hash.to_s + "---Стало"
