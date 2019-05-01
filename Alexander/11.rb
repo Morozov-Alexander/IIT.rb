9620858c994b17dbb36ad6547d9bef3cc3e2315f
Morozov-Alexander <morozov.alex.0001@gmail.com>
2019-05-01 21:17:26 +0300

    Delet files from the first project

--- Alexander/11.rb
+++
@@ -1,7 +0,0 @@
-#I guess that I shuld use gem"colorize" to solve this task
-#Дан массив в котором находятся строки "белый", "синий" и "красный" в случайном порядке. Необходимо упорядочить массив так, чтобы получился украинский флаг.
-# That's all I've came up  with. My brain is broken. Realy why not, Ukrainian - Yes!
-require 'colorize'
-array = ["красный","синий","белый"]
-puts array[0].light_yellow
-puts array[1].blue
